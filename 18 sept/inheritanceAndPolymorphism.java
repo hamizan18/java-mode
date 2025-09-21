210119a1317aaf3eea14b2c30e1520b4cbb9535f
Jaann <hamizanputra321@gmail.com>
2025-09-21 23:46:16 +0700

    Quest 7 Done!

--- 18 sept/inheritanceAndPolymorphism.java
+++ 18 sept/inheritanceAndPolymorphism.java
@@ -7,7 +7,11 @@ class Character {
         System.out.println(name + " menyerang!");
     }
     void showStatus() {
-        System.out.println("[" + name + "] \t\tHP: " + hp + " | Damage: " + power);
+        if (hp < 0) hp = 0;
+        if (hp > 0) 
+            System.out.println("[" + name + "] \nHP: " + hp + "\t| Damage: " + power);
+        else
+            System.out.println("[" + name + "] Die. ");
     }
 }
 
@@ -17,12 +21,11 @@ class Hero extends Character {
         this.name = name;
         this.hp = hp;
         this.power = atkPower;
-
         }
     @Override
     void attack(Character target) {
-        System.out.println(name + " menggunakan pedang! \t[" + power + "damage]");
-        enemy1.hp -= power;
+        System.out.println("[" + power + "damage] " + name + " menggunakan pedang ke arah " + target.name);
+        target.hp -= power;
     }
 }
 
@@ -35,7 +38,12 @@ class Enemy extends Character {
     }
     @Override
     void attack(Character target) {
-        System.out.println(name + " menggigit! \t\t[" + power + "damage]");
+        if (hp > 0){
+            System.out.println("[" + power + "damage] " + name + " menggigit!");
+            target.hp -= power;
+        } else {
+            System.out.println("...");
+        }
     }
 }
 
@@ -45,11 +53,12 @@ class Boss extends Enemy {
     }
     @Override
     void attack(Character target) {
-        System.out.println("Boss menghantam lantai! \t\t[" + power + "damage]");
-    }
-    int bossHit(Character target) {
-        hero1.hp -= bossDamage;
-        return hero1.hp;
+        if (hp > 0){
+        System.out.println("[" + power + "damage] " + name + " menghantam lantai!\n");
+        target.hp -= power;
+        } else {
+            System.out.println("...\n");
+        }
     }
 }
 
@@ -58,27 +67,56 @@ public class inheritanceAndPolymorphism {
     public static int randomDamage(int min, int max) {
         return (int)(Math.random()* (max - min + 1)) + min;
     }
+        static void delay() {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     public static void main(String[] args) {
 
+
         int enemyDamage = randomDamage(10, 20);
-        int heroDamage = randomDamage(15, 25);
-        int bossDamage = randomDamage(18, 20);
+        int heroDamage = randomDamage(30, 45);
+        int bossDamage = randomDamage(18, 30);
 
         // Inisialisasi Object
-        Hero hero1 = new Hero("Mingjaan", 100, heroDamage);
+        Hero hero1 = new Hero("Mingjaan", 150, heroDamage);
         Enemy enemy1 = new Enemy("Demon", 80, enemyDamage);
-        Boss boss = new Boss("Lord", 250, bossDamage);
+        Boss boss = new Boss("Lord", 200, bossDamage);
 
         // Print Status
         hero1.showStatus();
-        enemy1.showStatus();
         boss.showStatus();
+        enemy1.showStatus();
 
-        System.out.print("\n");
+        while(hero1.hp > 0 && (enemy1.hp > 0 || boss.hp > 0)){
 
-        // Print attack
-        hero1.attack(hero1);
-        enemy1.attack(enemy1);
+            System.out.print("\n");
+
+            // Print attack
+            hero1.attack(enemy1);
+            delay();
+            hero1.attack(boss);
+            delay();
+            enemy1.attack(hero1);
+            delay();
+            boss.attack(hero1);
+            delay();
+
+            // Print Status
+            hero1.showStatus();
+            delay();
+            enemy1.showStatus();
+            delay();
+            boss.showStatus();
+            delay();
+        }
+        if (hero1.hp == 0){
+            System.out.println("\n" + hero1.name + " Kalah.");
+        } else
+            System.out.println("\n" + enemy1.name + " & " + boss.name + " Kalah.");
     }
 }
